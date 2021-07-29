c34b8b9393b8f60f8d9bd39e2d58bed365171e4f
David Rawson <dm.rawson@gmail.com>
2021-07-29 21:07:38 +0100

    Initial commit. Model and controllers almost done

---
+++ src/controllers/todos/index.ts
@@ -0,0 +1,56 @@
+import { Response, Request } from "express";
+import { ITodo } from "../../types/todo";
+import Todo from "../../models/todo";
+
+const getTodos = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const todos: ITodo[] = await Todo.find();
+    res.status(200).json({ todos });
+  } catch (error) {
+    throw error;
+  }
+};
+
+const addTodo = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const body = req.body as Pick<ITodo, "name" | "description" | "status">;
+
+    const todo: ITodo = new Todo({
+      name: body.name,
+      description: body.description,
+      status: body.status,
+    });
+
+    const newTodo: ITodo = await todo.save();
+    const allTodos: ITodo[] = await Todo.find();
+
+    res
+      .status(201)
+      .json({ message: "Todo added", todo: newTodo, todos: allTodos });
+  } catch (error) {
+    throw error;
+  }
+};
+
+const updateTodos = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const {
+      params: { id },
+      body,
+    } = req;
+
+    const updateTodo: ITodo | null = await Todo.findByIdAndUpdate(
+      { _id: id },
+      body
+    );
+
+    const allTodos: ITodo[] = await Todo.find();
+    res.status(200).json({
+      message: "Todo updated",
+      todo: updateTodo,
+      todos: allTodos,
+    });
+  } catch (error) {
+    throw error;
+  }
+};
