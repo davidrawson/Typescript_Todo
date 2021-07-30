00a78a9476e195ba818de2ffdf3fb80fdf8d149c
David Rawson <dm.rawson@gmail.com>
2021-07-30 21:07:45 +0100

    Moved back end files that were in a very wrong place. Two react components and App.tsc worked on

--- todo-app/src/App.tsx
+++ todo-app/src/App.tsx
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import TodoItem from "./components/TodoItem";
+import AddTodo from "./components/AddTodo";
+import { getTodos, addTodo, updateTodo, deleteTodo } from "./API";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => {
+  const [todos, setTodos] = useState<ITodo[]>([]);
 
-export default App;
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  const fetchTodos = (): void => {
+    getTodos()
+      .then(({ data: { todos } }: ITodo[] | any) => setTodos(todos))
+      .catch((err: Error) => console.log(err));
+  };
+
+  const handleSaveTodo = (e: React.FormEvent, formData: ITodo): void => {
+    e.preventDefault();
+    addTodo(formData)
+      .then(({ status, data }) => {
+        if (status !== 201) {
+          throw new Error("Error. Todo not saved");
+        }
+        setTodos(data.todos);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const handleUpdateTodo = (todo: ITodo): void => {
+    updateTodo(todo)
+      .then(({ status, data }) => {
+        if (status !== 200) {
+          throw new Error("Error. Todo not updated");
+        }
+        setTodos(data.todos);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const handleDeleteTodo = (_id: string): void => {
+    deleteTodo(_id)
+      .then(({ status, data }) => {
+        if (status !== 200) {
+          throw new Error("Error. Todo not deleted");
+        }
+        setTodos(data.todos);
+      })
+      .catch((err) => console.log(err));
+  };
+};
